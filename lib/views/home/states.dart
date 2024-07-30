409848838bd65c0c3af00b73bacee546a44ce3d3
Abdelhady <mohamedabdelhady021@gmail.com>
2024-07-30 20:26:56 +0300

    push all files

---
+++ lib/views/home/states.dart
@@ -0,0 +1,13 @@
+class AddNotesStates {}
+
+class NotesInitialState extends AddNotesStates {}
+
+class AddNoteLoading extends AddNotesStates {}
+
+class AddNoteSuccess extends AddNotesStates {}
+
+class AddNoteFailed extends AddNotesStates {
+  final String errorMsg;
+
+  AddNoteFailed({required this.errorMsg});
+}
